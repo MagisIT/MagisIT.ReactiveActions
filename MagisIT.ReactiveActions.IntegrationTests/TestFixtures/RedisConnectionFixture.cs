131d0e2b55c6826f315ac2249c4b33e0dd862b77
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2019-05-07 03:04:48 +0200

    - CI configuration added

--- MagisIT.ReactiveActions.IntegrationTests/TestFixtures/RedisConnectionFixture.cs
+++ MagisIT.ReactiveActions.IntegrationTests/TestFixtures/RedisConnectionFixture.cs
@@ -11,11 +11,9 @@ namespace MagisIT.ReactiveActions.IntegrationTests.TestFixtures
 
         public RedisConnectionFixture()
         {
-            string host = Environment.GetEnvironmentVariable("REDIS_HOST") ?? throw new Exception("REDIS_HOST environment variable missing");
-            int port = int.TryParse(Environment.GetEnvironmentVariable("REDIS_PORT"), out int value)
-                ? value
-                : throw new Exception("REDIS_PORT environment variable invalid or missing");
-            string password = Environment.GetEnvironmentVariable("REDIS_PASSWORD") ?? throw new Exception("REDIS_PASSWORD environment variable missing");
+            string host = Environment.GetEnvironmentVariable("REDIS_HOST") ?? "localhost";
+            int port = int.TryParse(Environment.GetEnvironmentVariable("REDIS_PORT"), out int value) ? value : 6379;
+            string password = Environment.GetEnvironmentVariable("REDIS_PASSWORD");
 
             Connection = ConnectionMultiplexer.Connect(new ConfigurationOptions { EndPoints = { { host, port } }, Password = password });
         }
